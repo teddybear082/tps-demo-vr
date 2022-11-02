53089d1bb9dbfd3f139ee52ccf804ddb62654f94
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-11-02 17:04:48 -0400

    Complete implementation of left hand wrist menu and poke functionality
    -Also update to latest master XR Tools 3
    
    -Add latest XRTools 3 crouch function to player
    
    -Add timer for menu inputs on hand menu to prevent unintended repeated button presses

--- menu/hand_menu_2D.gd
+++ menu/hand_menu_2D.gd
@@ -10,15 +10,24 @@ signal teleport_button_pressed(state)
 signal seated_button_pressed(state)
 signal hand_menu_button_pressed
 
+onready var menu_cooldown = $MenuCooldown
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	if $MenuCooldown.time_left == 0:
+		for child in $Controls.get_children():
+			if child is Button:
+				child.disabled = false
+	
+	else:
+		for child in $Controls.get_children():
+			if child is Button:
+				child.disabled = true
 
 func _on_Controls_pressed():
 	$Main.visible = false
@@ -32,19 +41,19 @@ func _on_Exit_pressed():
 
 func _on_RobotBodyCheckButton_toggled(button_pressed):
 	emit_signal("robot_body_button_pressed", button_pressed)
-
+	$MenuCooldown.start()
 
 func _on_TeleportCheckButton_toggled(button_pressed):
 	emit_signal("teleport_button_pressed", button_pressed)
-
+	$MenuCooldown.start()
 
 func _on_SeatedModeCheckButton_toggled(button_pressed):
 	emit_signal("seated_button_pressed", button_pressed)
-
+	$MenuCooldown.start()
 
 func _on_SmoothTurnCheckButton_toggled(button_pressed):
 	emit_signal("smooth_turn_button_pressed", button_pressed)
-
+	$MenuCooldown.start()
 
 func _on_BackButton_pressed():
 	$Controls.visible = false
